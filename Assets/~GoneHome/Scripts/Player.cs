68c78381ad616bc45526b59408c3d0d69c561d9e
angus.rodwell <angus.rodwell@600900J211DE415>
2018-04-10 17:59:20 +1000

    We starting writing player script

--- Assets/~GoneHome/Scripts/Player.cs
+++ Assets/~GoneHome/Scripts/Player.cs
@@ -2,18 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ [] - Brackets
+ {} - Braces
+ () - Parethesis
+ 
+    Clean Code CTRL + K +D
+
+*/
 public class Player : MonoBehaviour
 {
-
+    public float moveSpeed = 3f;
     // Use this for initialization
     void Start()
     {
-
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+        transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            print("Hello World!");
+        }
+        
     }
+
 }
