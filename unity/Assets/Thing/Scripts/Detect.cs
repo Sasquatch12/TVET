296b69a245f489423c741bcb99281e076040e8e6
angus.rodwell <angus.rodwell@600900J211DE415>
2018-06-05 18:03:09 +1000

    Did RayCast

---
+++ unity/Assets/Thing/Scripts/Detect.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Detect : MonoBehaviour
+{
+    // Update is called once per frame
+    private Collider2D[] col;
+    void Update ()
+    {
+        if(!Input.GetMouseButton(0))
+        {
+            GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        ScreenMouseRay();
+	}
+    public void ScreenMouseRay()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 5;
+        Vector2 v = Camera.main.ScreenToWorldPoint(mousePosition);
+        col = Physics2D.OverlapPointAll(v);
+        if(col.Length > 0)
+        {
+            foreach (Collider2D c in col)
+            {
+                if(c.CompareTag("Button"))
+                {
+                    if (!Input.GetMouseButton(0))
+                    {
+                        c.GetComponent<SpriteRenderer>().color = Color.green;
+                    }
+                    else if(Input.GetMouseButtonDown(0))
+                    {
+                        c.GetComponent<SpriteRenderer>().color = Color.cyan;
+                        Debug.Log("Play");
+                        SceneManager.LoadScene(1);
+                    }
+                }
+            }
+        }
+    }
+}
