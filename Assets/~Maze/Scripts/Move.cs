8f662bfbe83cdffedaaa8b924850c0e1f001d28b
angus.rodwell <angus.rodwell@600900J211DE415>
2018-06-19 17:07:59 +1000

    Added all changes to GitHub

---
+++ Assets/~Maze/Scripts/Move.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour 
+{
+	public Rigidbody2D rb;
+	public float speed;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+
+			rb.MovePosition (transform.position + -transform.right * speed);
+			/*Vector3 position = this.transform.position;
+			position.x--;
+			this.transform.position = position;*/
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			rb.MovePosition (transform.position + transform.right * speed);
+			/*Vector3 position = this.transform.position;
+			position.x++;
+			this.transform.position = position;*/
+		}
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			rb.MovePosition (transform.position + transform.up * speed);
+			/*Vector3 position = this.transform.position;
+			position.y++;
+			this.transform.position = position;*/
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			rb.MovePosition (transform.position + -transform.up * speed);
+			/*Vector3 position = this.transform.position;
+			position.y--;
+			this.transform.position = position;*/
+		}
+	}
+	public void OnTriggerEnter2D(Collider2D collider)
+	{
+		if(collider.gameObject.CompareTag("Goal"))
+		{
+			Debug.Log ("Hit");
+			Application.LoadLevel("Maze2");
+		}
+	}
+}
---
+++ Assets/~Maze/Scripts/Text.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text : MonoBehaviour 
+{
+	public float time = 5;
+	// Use this for initialization
+	void Start () 
+	{
+		Destroy(gameObject, time);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
